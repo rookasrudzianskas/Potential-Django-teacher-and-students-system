596071b30a7609d67be0583eeff4ced6fdc9db80
Rokas <rookas.rudzenskas@gmail.com>
2021-04-20 16:38:34 +0300

    Rokas

---
+++ src/demo/urls.py
@@ -0,0 +1,12 @@
+from django.contrib import admin
+from django.urls import path, include
+from django.conf import settings
+from .views import home
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', home, home="home"),
+]
+
+if settings.DEBUG:
+    import debug_toolbar
+    urlpatterns += [path('__debug__/', include(debug_toolbar.urls))]
